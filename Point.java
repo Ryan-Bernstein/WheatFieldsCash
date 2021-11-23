cda7bd3f6914283ae94ae85b39475ff6050e353b
RYAN BERNSTEIN <78111880+Ryan-Bernstein@users.noreply.github.com>
2021-11-23 15:49:58 +0000

    point class

---
+++ Point.java
@@ -0,0 +1,12 @@
+public class Point{
+    private double x;
+    private double y;
+    
+    public Point(double _x, double _y){
+        x = _x;
+        y = _y;
+    }
+
+    public void setX(double _x){x = _x;}
+    public void setY(double _y){y = _y;}
+}
