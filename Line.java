00f58fd23fbf1fc52ea9867905085b20c4c2ed88
RYAN BERNSTEIN <78111880+Ryan-Bernstein@users.noreply.github.com>
2021-11-24 15:14:41 +0000

    Line method update

--- Line.java
+++ Line.java
@@ -3,7 +3,19 @@ public class Line{
     double length;
     double angle;
 
-    public draw(){
+    public Line(Point _origin, double _length, double _angle){
+        orgPoint = _origin;
+        length = _length;
+        angle = _angle;
+    }
+
+    public Point findB(){
+        double addX = Math.cos(angle) * this.length;
+        double addY = Math.sin(angle) * this.length;
+        Point out = new Point(addX + orgPoint.getX(),addY + orgPoint.getY());
+        return out;
+    }
+    public void draw(){
         
     }
 }
--- Point.java
+++ Point.java
@@ -9,4 +9,7 @@ public class Point{
 
     public void setX(double _x){x = _x;}
     public void setY(double _y){y = _y;}
+
+    public double getX(){return x;}
+    public double getY(){return y;}
 }
